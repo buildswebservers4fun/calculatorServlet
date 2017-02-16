c3d6452cd5db0c04ad420efaf4085c244b90fc33
CJ Miller <cjmiller721@gmail.com>
2017-02-16 17:31:33 -0500

    Implemented

--- src/main/java/calculatorHandlers/CalculatorServlet.java
+++ src/main/java/calculatorHandlers/CalculatorServlet.java
@@ -3,18 +3,12 @@ package calculatorHandlers;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import dynamic.IServlet;
-import dynamic.PluginRouter;
 import protocol.HttpRequest;
 import protocol.Protocol;
 import protocol.response.HttpResponseBuilder;
 import protocol.response.IHttpResponse;
-import calculatorPlugin.PluginLoader;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Map;
 
 /**
  * Created by Davis on 2/12/2017.
@@ -24,29 +18,47 @@ public class CalculatorServlet implements IServlet {
     public CalculatorServlet() {
     }
 
+
+    public IHttpResponse calc(HttpRequest request) {
+        if(request.getBody().length == 0) {
+            return null;
+        }
+
+        String body = new String(request.getBody());
+
+        Gson gson = new Gson();
+        try {
+            CalcRequest toCalc = gson.fromJson(body, CalcRequest.class);
+
+            return build200Response(gson.toJson(new CalcResponse(toCalc.first + toCalc.second)));
+        } catch (JsonSyntaxException e) {
+            return null;
+        }
+    }
+
     @Override
     public IHttpResponse handleDelete(HttpRequest request) {
-        return build404Response();
+        return calc(request);
     }
 
     @Override
     public IHttpResponse handleGet(HttpRequest request) {
-        return build404Response();
+        return calc(request);
     }
 
     @Override
     public IHttpResponse handleHead(HttpRequest request) {
-        return build404Response();
+        return calc(request);
     }
 
     @Override
     public IHttpResponse handlePost(HttpRequest request) {
-        return build404Response();
+        return calc(request);
     }
 
     @Override
     public IHttpResponse handlePut(HttpRequest request) {
-        return build404Response();
+        return calc(request);
     }
 
     private IHttpResponse build200Response(String body) {
@@ -56,16 +68,6 @@ public class CalculatorServlet implements IServlet {
         responseBuilder.setHeaders(new HashMap<String, String>());
         responseBuilder.setConnection(Protocol.CLOSE);
         responseBuilder.setBody(body);
-        return responseBuilder.build();
-    }
-
-    private IHttpResponse build404Response() {
-        HttpResponseBuilder responseBuilder = new HttpResponseBuilder();
-        responseBuilder.setStatus(Protocol.NOT_FOUND_CODE);
-        responseBuilder.setPhrase(Protocol.NOT_FOUND_TEXT);
-        responseBuilder.setHeaders(new HashMap<String, String>());
-        responseBuilder.setConnection(Protocol.CLOSE);
-        responseBuilder.setBody("File");
         return responseBuilder.build();
     }
 }
